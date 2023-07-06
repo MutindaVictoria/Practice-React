a5aabc6e21a856e6a44486f83921e7e3cdb3790d
MutindaVictoria <victoriaamutinda@gmail.com>
2023-07-06 07:35:43 +0300

    first commit

---
+++ src/Contents/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+
+const Contents= () => {
+  return (
+    <div>
+      <h3>What I Love about Gym</h3>
+      <p>It helps in boosting our health</p>
+      <p>It enables us Stay fit</p>
+
+      <h4>Gym chest Workout</h4>
+      <ul>
+      <li>Push-ups</li>
+  <li>Bench press</li>
+  <li>Dumbbell flyes</li>
+  <li>Incline bench press</li>
+  <li>Chest dips</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contents;
---
+++ src/Introduction/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './index.css';
+
+const Introduction = () => {
+  return (
+    <div>
+      <h1>What I Learnt in React</h1>
+      <p>It is a JavaScript library developed by Facebook</p>
+      <p>It has reusable components</p>
+
+      <h2>Gym Time Plan for Abs Workout</h2>
+      <ul>
+        <li>Warm-up: 5 minutes of jogging or cycling</li>
+        <li>Plank: Hold for 30 seconds, repeat 3 times</li>
+        <li>Crunches: 3 sets of 15 repetitions</li>
+        <li>Leg Raises: 3 sets of 12 repetitions</li>
+        <li>Russian Twists: 3 sets of 10 repetitions</li>
+        <li>Mountain Climbers: 3 sets of 20 repetitions</li>
+        <li>Cooldown: 5 minutes of stretching</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Introduction;
